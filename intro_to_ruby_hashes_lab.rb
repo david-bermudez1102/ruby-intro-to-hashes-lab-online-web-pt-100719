4e8986ea6fcfcff27bfa9bc3d755e63f44c88d3d
David Bermudez <juan.bermudez1102@outlook.com>
2019-10-09 18:27:28 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -30,10 +30,10 @@ def monopoly_with_third_tier
       :pieces => 4,
       :names => {
         :reading_railroad => {
-          
+
         },
         :pennsylvania_railroad => {
-          
+
         },
         :b_and_o_railroad => {
 
